6faab3b5ea39e00c2f612ee34f86c5b079aa9c9f
Ziyang Fan <24S151013@stu.hit.edu.cn>
2025-05-29 20:17:18 +0800

    fix vision-tower dtype bug

--- llava/model/builder.py
+++ llava/model/builder.py
@@ -291,6 +291,7 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
             vision_tower.load_model(device_map=device_map)
         if device_map != "auto":
             vision_tower.to(device="cuda", dtype=torch.float16)
+        vision_tower.to(dtype=torch.float16)
         image_processor = vision_tower.image_processor
 
     if hasattr(model.config, "max_sequence_length"):
