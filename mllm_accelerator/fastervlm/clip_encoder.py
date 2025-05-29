a623f71900e47919f24ad354a03600c43aadedbb
Ziyang Fan <24S151013@stu.hit.edu.cn>
2025-05-29 20:15:39 +0800

    remove uneccessary code

--- mllm_accelerator/fastervlm/clip_encoder.py
+++ mllm_accelerator/fastervlm/clip_encoder.py
@@ -17,14 +17,7 @@ def clip_vision_tower_feature_select(self, image_forward_outs):
     return image_features
 
 def clip_vision_tower_forward(self, images):
-    if type(images) is list:
-        image_features = []
-        for image in images:
-            image_forward_out = self.vision_tower(image.to(device=self.device, dtype=self.dtype).unsqueeze(0), output_attentions=True, output_hidden_states=True)
-            image_feature = self.feature_select(image_forward_out).to(image.dtype)
-            image_features.append(image_feature)
-    else:
-        image_forward_outs = self.vision_tower(images.to(device=self.device, dtype=self.dtype), output_attentions=True, output_hidden_states=True)
-        image_features = self.feature_select(image_forward_outs).to(images.dtype)
+    image_forward_outs = self.vision_tower(images.to(device=self.device, dtype=self.dtype), output_attentions=True, output_hidden_states=True)
+    image_features = self.feature_select(image_forward_outs).to(images.dtype)
 
     return image_features
